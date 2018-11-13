f66f44f013e01cd54446dd9a3893776bbced3435
marcio sansone <msansone@gmail.com>
2018-11-13 17:16:53 +0000

    primeira versão funcional, salvando no oracle xe do docker.

--- WebServiceEJB/src/br/msansone/testEJB/Model/Mensagem.java
+++ WebServiceEJB/src/br/msansone/testEJB/Model/Mensagem.java
@@ -6,15 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 
 @Entity
 @Stateless
 public class Mensagem {
-
-
-	@Id 
-    @GeneratedValue(strategy=GenerationType.AUTO) 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String conteudo;
     
